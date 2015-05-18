3d0e43539054ba79a4b9d7864f951505ba523aea
pmeinhardt <paul.meinhardt@student.hpi.de>
2015-05-18 10:48:03 +0200

    Restore some handlers, views from previous project
    In particular:
    - auth#new
    - search#show
    - user#show
    - repo#new
    - repo#show
    
    Also, slightly improved navbar responsiveness.

--- handlers/web.py
+++ handlers/web.py
@@ -1,6 +1,12 @@
+import tornado.escape
 import tornado.web
 
-class BaseHandler(tornado.web.RequestHandler):
+from tornado.web import HTTPError, RequestHandler
+from tornado.web import authenticated
+
+from models import User, Repo
+
+class BaseHandler(RequestHandler):
     """Base class for all web front end handlers."""
 
     def get_current_user(self):
@@ -19,6 +25,76 @@ class HomeHandler(BaseHandler):
 
     def get(self):
         self.render("home/index.html")
+
+class SearchHandler(BaseHandler):
+    def get(self):
+        query = tornado.escape.url_unescape(self.get_argument("q", ""))
+
+        if query:
+            pattern = "%" + query + "%"
+            repos = (Repo.select().join(User).alias("user")
+                .where(Repo.name ** pattern))
+        else:
+            repos = []
+
+        self.render("search/show.html", query=query, repos=repos)
+
+class UserHandler(BaseHandler):
+    def get(self, username):
+        try:
+            user = User.select().join(Repo).where(User.name == username).get()
+            self.render("user/show.html", title=user.name, user=user)
+        except User.DoesNotExist:
+            raise HTTPError(404)
+
+class RepoHandler(BaseHandler):
+    def get(self, username, reponame):
+        try:
+            repo = (Repo.select().join(User).alias("user")
+                .where((User.name == username) & (Repo.name == reponame))
+                .get())
+            title = repo.user.name + "/" + repo.name
+            self.render("repo/show.html", title=title, repo=repo)
+        except Repo.DoesNotExist:
+            raise HTTPError(404)
+
+class CreateRepoHandler(BaseHandler):
+    @authenticated
+    def get(self):
+        user = self.current_user
+        title = "Create a new repository"
+        self.render("repo/new.html", title=title, user=user)
+
+    @authenticated
+    def post(self):
+        reponame = self.get_argument("reponame", None)
+        desc = self.get_argument("description", None)
+        user = self.current_user
+        if not reponame:
+            self.redirect(self.reverse_url("web:create-repo"))
+            return
+        repo = Repo.create(user=user, name=reponame, desc=desc)
+        self.redirect(self.reverse_url("web:repo", user.name, repo.name))
+
+class SettingsHandler(BaseHandler):
+    pass
+
+class AuthHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.render("auth/new.html", title="Sign in - tailr")
+        else:
+            self.redirect("/")
+
+    def post(self):
+        user = User.get(User.name == self.get_argument("username"))
+        self.set_secure_cookie("uid", str(user.id))
+        self.redirect(self.get_argument("next", "/"))
+
+class DeauthHandler(BaseHandler):
+    def post(self):
+        self.clear_cookie("uid")
+        self.redirect("/")
 
 class ErrorHandler(BaseHandler):
     """Generates an error response with ``status_code`` for all requests."""
--- routes.py
+++ routes.py
@@ -5,16 +5,16 @@ import handlers.api
 
 routes = [
     url(r"/", handlers.web.HomeHandler, name="web:home"),
-    # url(r"/search", handlers.web.SearchHandler, name="web:search"),
+    url(r"/search", handlers.web.SearchHandler, name="web:search"),
     # url(r"/join", handlers.web.JoinHandler, name="web:join"),
-    # url(r"/auth", handlers.web.AuthHandler, name="web:auth"),
-    # url(r"/deauth", handlers.web.DeauthHandler, name="web:deauth"),
-    # url(r"/new", handlers.web.NewRepoHandler, name="web:new-repo"),
-    # url(r"/settings", handlers.web.SettingsHandler, name="web:settings"),
+    url(r"/auth", handlers.web.AuthHandler, name="web:auth"),
+    url(r"/deauth", handlers.web.DeauthHandler, name="web:deauth"),
+    url(r"/create", handlers.web.CreateRepoHandler, name="web:create-repo"),
+    url(r"/settings", handlers.web.SettingsHandler, name="web:settings"),
     # url(r"/settings/tokens/new", NewTokenHandler, name="new-token"),
     # url(r"/settings/tokens/([0-9]+)/del", DelTokenHandler, name="del-token"),
-    # url(r"/([^/]+)", handlers.web.UserHandler, name="web:user"),
-    # url(r"/([^/]+)/([^/]+)", handlers.web.RepoHandler, name="web:repo"),
+    url(r"/([^/]+)", handlers.web.UserHandler, name="web:user"),
+    url(r"/([^/]+)/([^/]+)", handlers.web.RepoHandler, name="web:repo"),
     url(r"/api/([^/]+)/([^/]+)", handlers.api.RepoHandler, name="api:repo"),
     url(r".*", handlers.web.ErrorHandler, dict(status_code=404)), # catch all
 ]
@@ -26,8 +26,8 @@ routes = [
 # GET   /auth                           Sign in form, provide credentials
 # POST  /auth                           Sign in, create session
 # POST  /deauth                         Sign out, destroy session
-# GET   /new                            Enter information for new repository
-# POST  /new                            Create new repository
+# GET   /create                         Enter information for new repository
+# POST  /create                         Create new repository
 # GET   /settings                       Account settings (tokens etc.)
 # GET   /settings/tokens/new            Provide information for a new API token
 # POST  /settings/tokens/new            Generate a new API token
@@ -35,7 +35,10 @@ routes = [
 # GET   /:user                          User page
 # GET   /:user/:repo                    Repository access
 
-# GET   /:user/:repo?index=true
+# PUT   /:user/:repo?key=URI
+# PUT   /:user/:repo?key=URI&datetime=DATETIME
+
 # GET   /:user/:repo?key=URI
-# GET   /:user/:repo?key=URI&datetime=TIMESTAMP
+# GET   /:user/:repo?key=URI&datetime=DATETIME
 # GET   /:user/:repo?key=URI&timemap=true
+# GET   /:user/:repo?index=true
